1a666e5a564b67abb8bf5e44270f825c93d34429
hetalp1698 <hetalp1698@gmail.com>
2021-02-28 14:36:48 -0500

    leaderboard component

--- would-you-rather/src/components/Leaderboard.js
+++ would-you-rather/src/components/Leaderboard.js
@@ -4,9 +4,38 @@ import { connect } from 'react-redux'
 class Leaderboard extends Component {
   render() {
     return (
-      <div>Leaderboard Component</div>
+      <div>
+        {this.props.leaderboardResults.map((user) => (
+          <div className="card" key={user.id}>
+            <img src={user.avatarURL} alt="Avatar" className="avatar" />
+            <div className="userInfo">
+              <h4 className="leader-title">{user.name}</h4>
+              <p className="question-info">Questions Asked: {user.questionsAsked}</p>
+              <p className="question-info">Questions Answered: {user.questionsAnswered}</p>
+              <h4 className="score-title">
+                Score: <span className="score-card">{user.userScore}</span>
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
     )
   }
 }
 
-export default connect()(Leaderboard);
+function mapStateToProps({users}) {
+  const usersList = Object.values(users)
+  const leaderboardResults = usersList.map((user) => ({
+    id: user.id,
+    name: user.name,
+    avatarURL: user.avatarURL,
+    questionsAsked: user.questions.length,
+    questionsAnswered: Object.keys(user.answers).length,
+    userScore: user.questions.length + Object.keys(user.answers).length
+  })).sort((a, b) => b.userScore - a.userScore)
+  return {
+    leaderboardResults
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard);
--- would-you-rather/src/utils/_DATA.js
+++ would-you-rather/src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: 'add url', // add the avatar url
+    avatarURL: 'https://github.com/hetaalll/React-Developer-Program/blob/main/would-you-rather/src/images/avatar1.png',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: 'add url', // add the avatar url
+    avatarURL: 'https://github.com/hetaalll/React-Developer-Program/blob/main/would-you-rather/src/images/avatar2.png',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: 'add url', // add the avatar url
+    avatarURL: 'https://github.com/hetaalll/React-Developer-Program/blob/main/would-you-rather/src/images/avatar3.png',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
